131d273d88a62fbc4eb3cf4f2f46d7e7bfe2550a
gusvaldev <gavq0603@gmail.com>
2024-02-23 16:56:04 -0700

    primer commit

---
+++ src/components/error.jsx
@@ -0,0 +1,12 @@
+
+const Error = ({mensaje}) => {
+    return (
+      <div className='font-bold mb-3 bg-red-800 text-white text-center p-3 rounded-xl uppercase'>
+                  
+      <p>{mensaje}</p>
+      
+  </div>  
+      )
+  }
+  
+  export default Error
