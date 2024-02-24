c5703b95c418f73bd1170ee07a020dd41312ac54
gusvaldev <gavq0603@gmail.com>
2024-02-23 17:20:34 -0700

    banquetes title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ function Header(){
     return(
         
         <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
-             Proximos Eventos{''}  
-            <span className="text-indigo-600">Veterinaria</span>
+             Proximos Eventos {''}  
+            <span className="text-indigo-600">Banquetes</span>
         </h1>
         
     )
